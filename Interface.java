9eadd5ab77f5f599382da97907df3417df14760d
Sand3sh77 <subedisandesh0123@gmail.com>
2026-01-04 08:27:11 +0545

    Add multiplication functionality to T1 class and define Mul interface

--- Interface.java
+++ Interface.java
@@ -2,9 +2,17 @@ interface Sum {
     void add(int x, int y);
 }
 
-class T1 implements Sum {
+interface Mul {
+    void multiply(int x, int y);
+}
+
+class T1 implements Sum, Mul {
     public void add(int x, int y) {
         System.out.println("Sum is: " + (x + y));
+    }
+
+    public void multiply(int x, int y) {
+        System.out.println("Product is: " + (x * y));
     }
 
     void display() {
@@ -16,6 +24,7 @@ public class Interface {
     public static void main(String[] args) {
         T1 obj = new T1();
         obj.add(5, 10);
+        obj.multiply(5, 10);
         obj.display();
     }
 }
