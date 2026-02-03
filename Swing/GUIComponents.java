cca5ac9b9493d4cd55edeaa14154083b1601f2c7
Sand3sh77 <subedisandesh0123@gmail.com>
2026-02-03 08:57:47 +0545

    GUIComponents added with Label, Textfield, CheckBox, RadioButton, ButtonGroup, ComboBox

--- Swing/GUIComponents.java
+++ Swing/GUIComponents.java
@@ -8,10 +8,41 @@ public class GUIComponents {
         JFrame frame = new JFrame("GUI Components Example");
         frame.setSize(500, 500);
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        frame.setLayout(new FlowLayout());
         JLabel l1 = new JLabel("User name:");
         JTextField t1 = new JTextField();
+
+        JCheckBox c1 = new JCheckBox("Java");
+        JCheckBox c2 = new JCheckBox("C++");
+        ButtonGroup bg1 = new ButtonGroup();
+        bg1.add(c1);
+        bg1.add(c2);
+
+        JRadioButton r1 = new JRadioButton("Male");
+        JRadioButton r2 = new JRadioButton("Female");
+        ButtonGroup bg2 = new ButtonGroup();
+        bg2.add(r1);
+        bg2.add(r2);
+
+        String languages[] = { "Java", "Python", "C++", "JavaScript" };
+        JComboBox cb1 = new JComboBox<>(languages);
+
+        // l1.setBounds(20, 50, 100, 20);
+        // t1.setBounds(120, 50, 100, 30);
+
+        frame.add(l1);
+        frame.add(t1);
+        frame.add(c1);
+        frame.add(c2);
+        frame.add(r1);
+        frame.add(r2);
+        frame.add(cb1);
+
+        frame.setVisible(true);
+
+        frame.requestFocus();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
